e3666f37ed367ac83cca39a90c8c447db3183800
mmeah21 <maevms@live.com>
2022-07-19 18:17:37 -0400

    FUNCTION added Clear Delete and Digit to display

--- scripts.js
+++ scripts.js
@@ -21,3 +21,49 @@ function divide(first,second){
 function operate(operator,first,second){
     return operator(first,second);
 }
+
+// assign a variable for the display and input values
+
+let input = [0];
+let storedValue = 0;
+
+const output = document.querySelector('#output');
+    output.innerHTML = input;
+
+//set click event to all buttons trigger 
+
+const numbers = document.querySelectorAll("button.digits");
+
+numbers.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.id);
+        input = input+button.id;
+        console.log("input: "+input);
+        display();
+    });
+});
+
+const clear = document.querySelector("#clear");
+clear.addEventListener('click', () => {
+    console.log("clear");
+    input = 0;
+    display();
+});
+
+
+const back = document.querySelector("#delete");
+back.addEventListener('click', () => {
+    console.log("delete: "+input[input.length-1]);
+    input =input.substring(0,input.length -1);
+    display();
+});
+
+
+function display(){
+    output.innerHTML = input;
+}
+
+
+
+
+
